40f88179dc52ec22fe89dcea9fd85f99b2a2f392
Niklas Holmgren <niklas.holmgren@gmail.com>
2018-11-14 15:52:01 +0100

    Move the "not_self_threading" constraint to its own migration.

--- migrations/20181010171343_commits_thread.js
+++ migrations/20181010171343_commits_thread.js
@@ -1,7 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table("commits", (t) => {
     t.string("thread").references("id");
-    knex.schema.raw("ALTER TABLE commits CONSTRAINT ADD not_self_threading CHECK ( thread <> id )");
   });
 };
 
