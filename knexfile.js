612fd354672065ccc248980de816f6eb57049ed4
Niklas Holmgren <niklas.holmgren@gmail.com>
2018-12-30 01:11:26 +0100

    Remove knexfile.js and migrations.

--- knexfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: process.env.DATABASE_CLIENT || 'pg',
-  connection: process.env.DATABASE_URL,
-  useNullAsDefault: true,
-  searchPath: process.env.DATABASE_SEARCH_PATH,
-  migrations: {
-    directory: __dirname + '/migrations'
-  }
-};
--- migrations/20181114154724_not_self_threading.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.raw("ALTER TABLE commits ADD CONSTRAINT not_self_threading CHECK ( thread <> id )");
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.raw("ALTER TABLE commits DROP CONSTRAINT not_self_threading");
-};
